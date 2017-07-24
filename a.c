8bc0f3d5f9878a6492d66d9e7b4c1db6eda2f768
arunperukudi <arunperukudi@gmail.com>
2017-07-24 09:45:47 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the value of character");
+scanf("%c",&ch);
+if(ch=='a'||ch==c||ch=='e'||ch=='b'||ch=='i')
+{
+printf("the value is vowels",ch);
+else
+{
+printf(" the value is not vowels",ch);
+}
+} return 0;
+}
